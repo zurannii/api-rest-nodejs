d4010640fc41f89345d5dd0fe8aaeed017828b34
Rob Nascimento <zuranmrs2@gmail.com>
2025-04-16 14:15:33 -0300

    iniciando o projeto com node.js e typescript

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import fastify from "fastify";
+
+const app = fastify()
+
+//GET, POST, PUT, PATCH, DELETE
+
+//http://localhost:3000/hello
+
+app.get('/hello', () => {
+    return 'Hello World!'
+}) 
+
+app.listen ({
+    port: 3333,
+}).then(() => {
+    console.log('HTTP server running!')
+})
