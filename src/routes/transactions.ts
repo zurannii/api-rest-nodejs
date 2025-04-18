b64db5e960540d1825714f7e1c687605d1e56640
Rob Nascimento <zuranmrs2@gmail.com>
2025-04-18 16:23:31 -0300

    fim das implementações de rotas

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -2,23 +2,49 @@ import { FastifyInstance } from "fastify"
 import { knex } from "../database"
 import { z } from "zod"
 import crypto from "node:crypto"
+import { request } from "node:http"
+import { error } from "node:console"
+import { checkSessionIdExists } from "../middleware/check-session-id-exists"
+import { console } from "node:inspector"
 
 export async function transactionsRoutes(app: FastifyInstance) {
+    app.addHook('preHandler', async (request, reply) => {
+        console.log(`[${request.method}] ${request.url}`)
+    })
 
-    app.get('/', async () => {
-        const transactions = await knex('transactions').select()
+    app.get('/',
+         {
+        preHandler: [checkSessionIdExists]
+         },
+     async (request, reply) => {
+        const { sessionId } = request.cookies
+
+        const transactions = await knex('transactions')
+        .where('session_id', sessionId)
+        .select()
         return { transactions }
     })
 
-    app.get('/summary', async () => {
+    app.get('/summary', 
+        {
+        preHandler: [checkSessionIdExists]
+         },
+          async (request, reply) => {
+            const { sessionId } = request.cookies
         const summary = await knex('transactions')
             .sum('amount', { as: 'amount' })
+            .where('session_id', sessionId)
             .first()
 
         return { summary }
     })
 
-    app.get('/:id', async (request) => {
+    app.get('/:id', 
+        {
+        preHandler: [checkSessionIdExists]
+        },
+          async (request, reply) => {
+            const { sessionId } = request.cookies
         const getTransactionParamsSchema = z.object({
             id: z.string().uuid(),
         })
@@ -26,7 +52,10 @@ export async function transactionsRoutes(app: FastifyInstance) {
         const { id } = getTransactionParamsSchema.parse(request.params)
 
         const transaction = await knex('transactions')
-            .where('id', id)
+            .where({
+                session_id: sessionId,
+                id,
+            })
             .first()
 
         return { transaction }
